1a869c7556898b317ebe16e08ce8317a918cc3f7
Venelin Efremov <veffremov.ve@gmail.com>
2016-06-18 17:32:36 -0700

    OBEX use system D-BUS.

--- obexd/src/manager.c
+++ obexd/src/manager.c
@@ -494,7 +494,7 @@ gboolean manager_init(void)
 
 	dbus_error_init(&err);
 
-	connection = g_dbus_setup_bus(DBUS_BUS_SESSION, OBEXD_SERVICE, &err);
+	connection = g_dbus_setup_bus(DBUS_BUS_SYSTEM, OBEXD_SERVICE, &err);
 	if (connection == NULL) {
 		if (dbus_error_is_set(&err) == TRUE) {
 			fprintf(stderr, "%s\n", err.message);
